404de6b6faeff7335486031197dd0e132759a415
Ayush Watnani <watnaniayush@gmail.com>
2022-08-03 15:22:25 +0530

    creted header of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crypto-app';
+  selectedCurrency: any;
+
+  constructor(){
+
+  }
+
+  sendCurrency(event : string){
+    console.log(event);
+  }
 }
